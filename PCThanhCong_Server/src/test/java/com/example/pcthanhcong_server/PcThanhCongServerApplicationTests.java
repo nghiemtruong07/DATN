05c74ef0d97f4319a3f1505602a1668040502236
Nghiêm Trường <truongchum2001@gmail.com>
2023-03-21 10:51:37 +0700

    cretae project

---
+++ PCThanhCong_Server/src/test/java/com/example/pcthanhcong_server/PcThanhCongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcthanhcong_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcThanhCongServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
