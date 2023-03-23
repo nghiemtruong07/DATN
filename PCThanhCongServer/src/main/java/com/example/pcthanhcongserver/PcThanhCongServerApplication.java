5eee94e162ad70a0a3f09d0d895a5a288ee5ece6
Nghiêm Trường <truongchum2001@gmail.com>
2023-03-23 09:32:52 +0700

    config file

---
+++ PCThanhCongServer/src/main/java/com/example/pcthanhcongserver/PcThanhCongServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcthanhcongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcThanhCongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcThanhCongServerApplication.class, args);
+    }
+
+}
