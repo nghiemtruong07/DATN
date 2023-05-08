eedbac1c040f8a9cc403d4b7260a670ab30be3a5
Nghiêm Trường <truongchum2001@gmail.com>
2023-05-08 18:55:06 +0700

    submmit

--- FE/datn-fe/src/api/PaymenApi.js
+++
@@ -1,6 +0,0 @@
-import Instance from '../axios/Instance'
-
-export const getPaypalPayment = (data) =>{
-    const url = `/api/site/payment`;
-    return Instance.post(url, data);
-}
--- FE/datn-fe/src/api/SaleApi.js
+++
@@ -1,6 +0,0 @@
-import Instance from '../axios/Instance'
-
-export const getSaleDetail = (id) =>{
-    const url = `/api/admin/sale/detail/${id}`;
-    return Instance.get(url);
-}
