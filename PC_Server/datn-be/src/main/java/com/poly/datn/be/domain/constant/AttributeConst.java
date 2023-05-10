9165666a83868eb4891e5a1ebe8084150b44d909
Nghiêm Trường <truongchum2001@gmail.com>
2023-05-10 20:08:53 +0700

    update Admin

--- PC_Server/datn-be/src/main/java/com/poly/datn/be/domain/constant/AttributeConst.java
+++ PC_Server/datn-be/src/main/java/com/poly/datn/be/domain/constant/AttributeConst.java
@@ -3,7 +3,7 @@ package com.poly.datn.be.domain.constant;
 public class AttributeConst {
     /* Attribute constant */
     public static final String ATTRIBUTE_MSG_ERROR_NOT_EXIST = "Mã sản phẩm không tồn tại!";
-    public static final String ATTRIBUTE_MSG_ERROR_NOT_ENOUGH_STOCK = "Số lượng sản phẩm không đủ!";
+    public static final String ATTRIBUTE_MSG_ERROR_NOT_ENOUGH_STOCK = "Sản phẩm đã hết hàng!";
     public static final Integer ATTRIBUTE_CACHE_INIT = 0;
     /* Url Api */
     public static final String API_ATTRIBUTE_GET_BY_PRODUCT_ID = "/api/site/attribute/get-by-product";
